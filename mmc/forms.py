b18d603f7745c04e71a5f3f9ffd6a3a390838e9f
Jacob Peddicord <jacob@peddicord.net>
2012-01-24 20:54:31 -0500

    Fix "null" file on cancel.

--- mmc/forms.py
+++ mmc/forms.py
@@ -2,6 +2,7 @@ from flask.ext.wtf import Form, Required, EqualTo, Length, Regexp, TextField, Pa
 
 
 class SiteForm(Form):
+    # TODO: validate to ensure it doesn't exist.
     domain = TextField("Domain", [Required(), Regexp("^[a-zA-Z0-9\-\.]+$", message="Not a valid domain name.")])
 
 class ChangePasswordForm(Form):
