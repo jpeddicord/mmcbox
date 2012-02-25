e6034554d6dd9231868b04bb36515f4af6bedb49
Jacob Peddicord <jacob@peddicord.net>
2012-02-25 14:23:46 -0500

    Add file logging.

--- app.py
+++ app.py
@@ -3,5 +3,14 @@ from mmc import models
 from mmc import views
 
 
+# logging setup
+if not app.debug:
+    from logging import Formatter, StreamHandler, WARNING, DEBUG
+    handler = StreamHandler()
+    handler.setFormatter(Formatter('%(asctime)s %(levelname)-7s %(name)s - %(message)s'))
+    app.logger.addHandler(handler)
+    app.logger.setLevel(WARNING)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
